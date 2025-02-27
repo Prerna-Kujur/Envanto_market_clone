83ca6bce2d767c23ec9306ff9260e30552d585f4
Prerna-Kujur <prernakujur002@gmail.com>
2025-02-27 10:50:00 +0530

    Initial commit with all files

---
+++ envato_market_clone-main/src/App.js
@@ -0,0 +1,16 @@
+import {Navbar,Home,Demo,Features,Elements,Fantastic} from "./components";
+
+function App() {
+  return (
+    <div className="bg-black h-screen w-full">
+      <Navbar />
+      <Home />
+      <Demo />
+      <Features />
+      <Elements />
+      <Fantastic />
+    </div>
+  );
+}
+
+export default App;
